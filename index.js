d4430be779c7c51a0ce12eea31ccc1a98c9aa20d
lyfeyaj <lyfeyaj@gmail.com>
2016-03-14 23:23:17 +0800

    fix bugs and add comments

--- index.js
+++ index.js
@@ -1,16 +1,32 @@
 'use strict';
 
-var SEPERATOR = /\[|\.|\]/;
+/*!
+ * Module variables
+ */
+
+var SEPERATOR = /\[['"]?|\.|['"]?\]/;
 var STRING_DETECTOR = '[object String]';
 var ARRAY_DETECTOR = '[object Array]';
+
+/*!
+ * Check if an object is a string
+ */
 
 function isString (str) {
   return Object.prototype.toString.call(str) === STRING_DETECTOR;
 }
 
+/*!
+ * Check if an object is an array
+ */
+
 function isArray (str) {
   return Object.prototype.toString.call(str) === ARRAY_DETECTOR;
 }
+
+/*!
+ * Check if a string is a positive integer
+ */
 
 function isInteger (str) {
   return /^(0|[1-9]\d*)$/.test(str);
@@ -20,6 +36,9 @@ function isNil (obj) {
   return obj !== obj || obj === undefined || obj === null;
 }
 
+/*!
+ * Remove falsy values from an array
+ */
 function compact (array) {
   array = array || [];
   return array.filter(function(el) {
@@ -27,22 +46,30 @@ function compact (array) {
   });
 }
 
+/*!
+ * Eval an object for specific action type: `get`, `set`, `remove`, `exists`
+ */
+
 function _eval (type, obj, path, value) {
+  // Parse string path into an array
   if (isString(path)) {
     path = compact(path.trim().split(SEPERATOR));
   }
 
+  // Return if path is invalid
   if (!path || !isArray(path) || path.length === 0) return;
 
+  // Copy array for future use
   path = path.slice();
 
+  // Shift the first path value
   var key = path.shift();
 
-  // console.log(type, obj, isNil(obj), path);
+  // Return `undefined` if obj is `NaN` or `null` or `undefined`
   if (isNil(obj)) return;
+
   switch (type) {
     case 'get':
-
       if (path.length === 0) {
         return obj[key];
       }
@@ -94,48 +121,153 @@ function _eval (type, obj, path, value) {
   return _eval(type, obj[key], path, value);
 }
 
+/**
+ * ### Magico (object)
+ *
+ * @param {Object} object to which will be wrapped for later use
+ * @name Magico
+ * @api public
+ */
 function Magico(obj) {
   this._obj = obj;
 }
+
+/**
+ * ### Magico.wrap (object)
+ *
+ * @param {Object} object to which will be wrapped for later use
+ * @name Magico.wrap
+ * @return Magico instance
+ * @api public
+ */
 
 Magico.wrap = function (obj) {
   return new Magico(obj);
 };
 
+/**
+ * ### Magico.set (object, path, value)
+ *
+ * @param {Object} object to which will be wrapped for later use
+ * @param {String | Array} path to which will use to access object
+ * @param {Object} value to which will be set
+ * @name Magico.set
+ * @return {Boolean} whether the value is set or not
+ * @api public
+ */
+
 Magico.set = function (obj, path, value) {
   return !!_eval('set', obj, path, value);
 };
+
+/**
+ * ### Magico.get (object, path)
+ *
+ * @param {Object} object to which will be wrapped for later use
+ * @param {String | Array} path to which will use to access object
+ * @name Magico.get
+ * @return {Object} the value of properties
+ * @api public
+ */
 
 Magico.get = function (obj, path) {
   return _eval('get', obj, path);
 };
 
+/**
+ * ### Magico.exists (object, path)
+ *
+ * @param {Object} object to which will be wrapped for later use
+ * @param {String | Array} path to which will use to access object
+ * @name Magico.exists
+ * @return {Boolean} the value of path exists or not
+ * @api public
+ */
+
 Magico.exists = function (obj, path) {
   return !!_eval('exists', obj, path);
 };
+
+/**
+ * ### Magico.remove (object, path, value)
+ *
+ * @param {Object} object to which will be wrapped for later use
+ * @param {String | Array} path to which will use to access object
+ * @param {Object} value to which will be remove
+ * @name Magico.remove
+ * @return {Boolean} whether the value is removed or not
+ * @api public
+ */
 
 Magico.remove = function (obj, path) {
   return !!_eval('remove', obj, path);
 };
 
+/**
+ * ### Magico.prototype.set (path, value)
+ *
+ * @param {String | Array} path to which will use to access object
+ * @param {Object} value to which will be set
+ * @name Magico.prototype.set
+ * @return {Boolean} whether the value is set or not
+ * @api public
+ */
+
 Magico.prototype.set = function (path, value) {
   return Magico.set(this._obj, path, value);
 };
+
+/**
+ * ### Magico.prototype.get (path)
+ *
+ * @param {String | Array} path to which will use to access object
+ * @name Magico.prototype.get
+ * @return {Object} the value of properties
+ * @api public
+ */
 
 Magico.prototype.get = function (path) {
   return Magico.get(this._obj, path);
 };
 
+/**
+ * ### Magico.prototype.exists (path)
+ *
+ * @param {String | Array} path to which will use to access object
+ * @name Magico.prototype.exists
+ * @return {Boolean} the value of path exists or not
+ * @api public
+ */
+
 Magico.prototype.exists = function (path) {
   return Magico.exists(this._obj, path);
 };
+
+/**
+ * ### Magico.prototype.remove (path, value)
+ *
+ * @param {String | Array} path to which will use to access object
+ * @param {Object} value to which will be remove
+ * @name Magico.prototype.remove
+ * @return {Boolean} whether the value is removed or not
+ * @api public
+ */
 
 Magico.prototype.remove = function (path) {
   return Magico.remove(this._obj, path);
 };
 
+/**
+ * ### Magico.prototype.toObject
+ *
+ * @name Magico.prototype.toObject
+ * @return {Object} the changed object
+ * @api public
+ */
+
 Magico.prototype.toObject = function () {
   return this._obj;
 };
 
+// Export Magico function
 module.exports = Magico;
